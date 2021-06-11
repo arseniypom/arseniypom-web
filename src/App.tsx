ca498c4603ba962fa0179c61413ec07e4fae20f8
arseniypom <arpomazkov@bk.ru>
2021-06-11 18:25:59 +0300

    Started the project, added navigation bar component and starting styles for it

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return <>
+    <Navbar />
+    <div className="container">
+      <h1>Hi,</h1>
+      <h1>I'm Arseniy,</h1>
+      <h1>web developer</h1>
+    </div>
+  </>;
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav className="main-menu">
+      <div className="main-menu_top">
+        <a className="logo" href="/">
+          <div className="main-menu_top_img"></div>
+        </a>
+      </div>
+    </nav>
+  )
+}
